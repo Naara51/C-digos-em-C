435543c7a6e07d68ed57909a1a683c57ea38ea23
Rebeca Naara <rebecanaara05@gmail.com>
2025-07-18 17:09:40 -0300

    Create Calculadora simples.cpp

---
+++ Calculadora simples.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main (){
+	
+	setlocale(LC_ALL,"Portuguese");
+	
+	int Opcao;
+	float Resultado,Numero1, Numero2;
+   
+	do{
+  
+	  	printf("Calculadora \n");
+		
+		printf("Digite uma operação\n");
+		printf("1- Soma; \n");
+		printf("2- Subtração: \n");
+		printf("3- Multiplicação: \n");
+		printf("4- Divisão; \n");
+		scanf("%d", &Opcao);
+		system("cls");
+	 	
+		if(Opcao <1 || Opcao >4){
+			printf("Opção inválida. \n");}
+		
+		else{
+		
+			printf("Digite um número: \n");
+			scanf("%f", &Numero1);
+			printf("Digite outro número; \n");
+			scanf("%f", &Numero2);
+			system("cls");}
+			
+	   	      switch(Opcao){
+			   
+				case 1 : 
+				 Resultado= Numero1 + Numero2;
+				 printf("O resultado será %.2f\n", Resultado);
+				  break;
+			    case 2 :
+			   	 Resultado= Numero1 - Numero2; 
+			   	 printf("O resultado será %.2f\n", Resultado);
+				  break;
+				case 3:
+				 Resultado = Numero1*Numero2;
+				 printf("O resultado será %.2f\n", Resultado);
+				  break;
+				case 4 :
+				 Resultado = Numero1/Numero2;
+				 printf("O resultado será %.2f\n", Resultado);
+				  break;}
+				system("pause");
+				system("cls");
+		
+	}while(1);
+	
+  
+return 0;	
+}
