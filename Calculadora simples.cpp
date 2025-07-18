a74cdc9da86644f47c3e4f817ab27aaae3b63fa2
Rebeca Naara <rebecanaara05@gmail.com>
2025-07-18 17:26:25 -0300

    Update Calculadora simples.cpp

--- Calculadora simples.cpp
+++ Calculadora simples.cpp
@@ -39,7 +39,7 @@ int main (){
 				 Resultado= Numero1 + Numero2;
 				 printf("O resultado será %.2f\n", Resultado);
 				  break;
-			    case 2 :
+			        case 2 :
 			   	 Resultado= Numero1 - Numero2; 
 			   	 printf("O resultado será %.2f\n", Resultado);
 				  break;
