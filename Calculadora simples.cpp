f4643d5d9f86d956b12dfe56c4481a6bc7438fea
Rebeca Naara <rebecanaara05@gmail.com>
2025-07-21 10:49:35 -0300

    Update Calculadora simples.cpp

--- Calculadora simples.cpp
+++ Calculadora simples.cpp
@@ -1,3 +1,6 @@
+/* Este programa executa uma caluladora com operações simples de soma, subtração, multiplicação e divisão, com estrutura de repetção do-while
+  e opção de saida ao usuário. Ele verifica se a opção é válida, solicita os valores caso seja e realiza o cálculo. */
+
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
@@ -12,48 +15,58 @@ int main (){
    
 	do{
   
-	  	printf("Calculadora \n");
+	  	printf("=== Calculadora === \n");
 		
-		printf("Digite uma operação\n");
 		printf("1- Soma; \n");
-		printf("2- Subtração: \n");
-		printf("3- Multiplicação: \n");
+		printf("2- Subtração; \n");
+		printf("3- Multiplicação; \n");
 		printf("4- Divisão; \n");
+		printf("5-Sair. \n");
+		printf("Digite uma opção: \n");
 		scanf("%d", &Opcao);
-		system("cls");
-	 	
-		if(Opcao <1 || Opcao >4){
+		
+		system("cls"); // Remova esta linha caso não esteja utilizando o windows
+
+		if (Opcao == 5){
+		printf("Encerrando o programa\n");
+			break;}
+	
+		if(Opcao <1 || Opcao >5){
 			printf("Opção inválida. \n");}
 		
 		else{
 		
 			printf("Digite um número: \n");
 			scanf("%f", &Numero1);
-			printf("Digite outro número; \n");
+			printf("Digite outro número: \n");
 			scanf("%f", &Numero2);
-			system("cls");}
+			system("cls");// Remova esta linha caso não esteja utilizando o windows
+		        }
 			
-	   	      switch(Opcao){
+	   	      	switch(Opcao){
 			   
-				case 1 : 
-				 Resultado= Numero1 + Numero2;
-				 printf("O resultado será %.2f\n", Resultado);
-				  break;
-			        case 2 :
-			   	 Resultado= Numero1 - Numero2; 
-			   	 printf("O resultado será %.2f\n", Resultado);
-				  break;
-				case 3:
-				 Resultado = Numero1*Numero2;
-				 printf("O resultado será %.2f\n", Resultado);
-				  break;
-				case 4 :
-				 Resultado = Numero1/Numero2;
-				 printf("O resultado será %.2f\n", Resultado);
-				  break;}
-				system("pause");
-				system("cls");
-		
+					case 1 : 
+				 		Resultado= Numero1 + Numero2;
+						printf("O resultado será %.2f\n", Resultado);
+				  		break;
+			                case 2 :
+			   	 		Resultado= Numero1 - Numero2; 
+			   	 		printf("O resultado será %.2f\n", Resultado);
+				  		break;
+					case 3:
+				 		Resultado = Numero1*Numero2;
+				 		printf("O resultado será %.2f\n", Resultado);
+				  		break;
+					case 4 :
+				 		if (Numero2==0){
+				  			printf("Erro divisão por 0 não permitida.\n");}
+			  	 		else{
+				  			Resultado = Numero1/Numero2;
+				  			printf("O resultado será %.2f\n", Resultado);}
+				  			break;}
+							   
+					system("pause"); // Remova esta linha caso não esteja utilizando o windows
+					system("cls"); // Remova esta linha caso não esteja utilizando o windows
 	}while(1);
 	
   
