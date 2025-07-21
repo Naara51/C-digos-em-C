0dd9fac4945da5478ca3901ba77b5bcd9dcbcaf5
Rebeca Naara <rebecanaara05@gmail.com>
2025-07-21 11:53:53 -0300

    Update Fatorial.cpp

--- Fatorial.cpp
+++ Fatorial.cpp
@@ -1,3 +1,5 @@
+// Este programa calcula o fatorial de um número inteiro utilizando a estrutura de repetição for, a variável i serve como contador.
+
 #include<stdio.h>
 #include<locale.h>
 
@@ -5,15 +7,20 @@ int main (){
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	int Numero, fatorial=1, i;
+	int Numero, i;
+	unsigned long long int fatorial=1;//Consegue guardar valores de até 64 bits
 	
-	printf("Digite um número: ");
+	printf("Digite um número inteiro: ");
 	scanf("%i", &Numero);
+	
+	if(Numero <0){
+	printf("Não existe fatorial de números negativos.");
+	return 1;}
 	
 	for(i=1;i<=Numero;i++){
 	fatorial=fatorial*i;}
 	
-	printf("O fatorial de %i será %i", Numero,fatorial);
+	printf("O fatorial de %i será %llu", Numero,fatorial);
 	
 	return 0;
 	
